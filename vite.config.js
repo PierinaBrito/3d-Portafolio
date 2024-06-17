4d802ab10b23ed91765a8b38f4d50625e12505e1
PIERINA BRITO <pierina.brito@tbiil.edu>
2024-06-17 18:20:43 -0500

    update vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
+  assetsInclude: ['**/*.glb'],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173, // Use the PORT environment variable or default to 3000
+  },
 })
+
+
